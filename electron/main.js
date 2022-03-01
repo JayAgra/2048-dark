9402352f23ed762d095e08a2c13c4b49ee6884e8
JayAgra <69493224+JayAgra@users.noreply.github.com>
2022-03-01 06:51:37 -0800

    mainWindow = fortyEight, app = twenty

--- electron/main.js
+++ electron/main.js
@@ -1,26 +1,26 @@
-const {app, BrowserWindow} = require('electron')
-let mainWindow
+const {twenty, BrowserWindow} = require('electron')
+let fortyEight
 
 function createWindow () {
   // create window
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  fortyEight = new BrowserWindow({width: 800, height: 600})
   //load index
-  mainWindow.loadFile('index.html')
-  mainWindow.on('closed', function () {
-    mainWindow = null
+  fortyEight.loadFile('index.html')
+  fortyEight.on('closed', function () {
+    fortyEight = null
   })
 }
-app.on('ready', createWindow)
+twenty.on('ready', createWindow)
 //stop when windows killed
-app.on('window-all-closed', function () {
+twenty.on('window-all-closed', function () {
 //macos quit
   if (process.platform !== 'darwin') {
-    app.quit()
+    twenty.quit()
   }
 })
-app.on('activate', function () {
+twenty.on('activate', function () {
 //reopen
-  if (mainWindow === null) {
+  if (fortyEight === null) {
     createWindow()
   }
 })
